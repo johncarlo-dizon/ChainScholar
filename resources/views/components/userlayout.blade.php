d896e1cf1043bcfa175d6a5209631d20c45eac91
johncarlo-dizon <johncarlovdizon@gmail.com>
2025-10-13 23:37:28 +0800

    title

--- resources/views/components/userlayout.blade.php
+++ resources/views/components/userlayout.blade.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+
+@php
+    $user = Auth::user();
+    $roleMap = ['ADMIN' => 'Admin', 'ADVISER' => 'Adviser', 'STUDENT' => 'Student'];
+    $roleLabel = $roleMap[$user->role ?? ''] ?? 'User';
+@endphp
+
+
+    <title>Chainscholar - {{ $roleLabel }} Portal</title>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
     <link rel="icon" href="{{ asset('storage/images/chainlogo.png') }}" type="image/png">
     @vite('resources/css/app.css')
